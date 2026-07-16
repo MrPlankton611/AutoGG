bf77f0b9dd4a0a52ec61042a381da19dccca9937
MrPlankton611 <shamgardavid0@gmail.com>
2026-07-16 17:59:17 -0400

    Restore server-address mango log

--- src/main/java/com/autogg/autogg/AutoGGMod.java
+++ src/main/java/com/autogg/autogg/AutoGGMod.java
@@ -1,16 +1,31 @@
 package com.autogg.autogg;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import net.fabricmc.api.ModInitializer;
 
 public class AutoGGMod implements ModInitializer {
     public static final String MOD_ID = "autogg";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    private static boolean hasLogged = false;
 
     @Override
     public void onInitialize() {
         LOGGER.info("AutoGG initialized!");
+
+        ClientTickEvents.START_WORLD_TICK.register(client -> {
+            if (hasLogged) return;
+
+            MinecraftClient mc = MinecraftClient.getInstance();
+            if (mc != null && !mc.isInSingleplayer() && mc.getCurrentServerEntry() != null) {
+                String address = mc.getCurrentServerEntry().address;
+                if ("play.chi-us.pvphq.com".equals(address)) {
+                    LOGGER.info("6767 mango mango");
+                    hasLogged = true;
+                }
+            }
+        });
     }
 }
